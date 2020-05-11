cf68b1f678dcfc970009b2732ab3d39ae9fc82d6
hervit0 <herve.ahleung@gmail.com>
2020-05-11 08:00:39 +0000

    Automated deployment to GitHub Pages: daceb7a7a391eb2e1d5f5af46af09953639f72dd

---
+++ precache-manifest.653f4343ab7d9116fc52e669f8350fb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646318477f06fba764017042c2246223",
+    "url": "/takamaka/index.html"
+  },
+  {
+    "revision": "f2b2cd19720a00eaf8e4",
+    "url": "/takamaka/static/css/main.05fa3424.chunk.css"
+  },
+  {
+    "revision": "29790feda5ec3a39d76c",
+    "url": "/takamaka/static/js/2.05f5a5a3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/takamaka/static/js/2.05f5a5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b2cd19720a00eaf8e4",
+    "url": "/takamaka/static/js/main.10a757cf.chunk.js"
+  },
+  {
+    "revision": "4a8abf376737bede4133",
+    "url": "/takamaka/static/js/runtime-main.619539ba.js"
+  },
+  {
+    "revision": "77943a6c8b506737d091f721f6f9169d",
+    "url": "/takamaka/static/media/begin-random.77943a6c.jpg"
+  },
+  {
+    "revision": "7895e492747c35b6bf409c3163024ab2",
+    "url": "/takamaka/static/media/begin.7895e492.jpg"
+  },
+  {
+    "revision": "e1b7ca6ad1f2349655324756d5febcb2",
+    "url": "/takamaka/static/media/book.e1b7ca6a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/takamaka/static/media/logo.5d5d9eef.svg"
+  }
+]);
