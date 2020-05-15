50fec682b8f4a3537b6d6fc57d22d350476d4edb
hervito <h.ah-leung@resilientplc.com>
2020-05-15 23:31:56 +0400

    Wire the card

--- src/pages/level/level9.tsx
+++ src/pages/level/level9.tsx
@@ -1,5 +1,5 @@
 import DateFnsUtils from '@date-io/date-fns';
-import { Box, Container } from '@material-ui/core';
+import { Box, Card, CardActionArea, CardContent, Container, Typography } from '@material-ui/core';
 import { MuiPickersUtilsProvider, TimePicker } from '@material-ui/pickers';
 import React from 'react';
 import Level from '../../components/level/level';
@@ -8,7 +8,7 @@ const Level9 = () => {
   const [selectedDate, handleDateChange]: [Date, any] = React.useState(new Date());
 
   return (
-    <Level name={'Time is running out'}>
+    <Level name={'Time is running out [WIP]'}>
       <Container maxWidth={'sm'}>
         <Box display={'flex'} justifyContent={'center'}>
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -23,8 +23,43 @@ const Level9 = () => {
           </MuiPickersUtilsProvider>
         </Box>
       </Container>
+
+      <Card>
+        <CardActionArea>
+          <CardContent>
+            <Box fontStyle="oblique">
+              <Typography variant="body1">
+                {getStoryLine(selectedDate.getHours(), selectedDate.getMinutes())}
+              </Typography>
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Level>
   );
+};
+
+const getStoryLine = (hours: number, minutes: number): string => {
+  switch (hours) {
+    case 1: {
+      return 'early';
+    }
+    case 2: {
+      return 'early';
+    }
+    case 19: {
+      switch (minutes) {
+        case 59: {
+          return 'BOOM!';
+        }
+        default:
+          return 'It is imminent';
+      }
+    }
+    default:
+      return 'The bomb has exploded';
+  }
+
 };
 
 export default Level9;
