3cc4c339805f7fc50da0ace800f98c52faeb9662
hervit0 <herve.ahleung@gmail.com>
2020-05-16 09:29:36 +0000

    Automated deployment to GitHub Pages: 843eef331860907fc1db36d00c19410b1307d3a2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/takamaka/precache-manifest.037e16d11340f49a26dcfecda75472a2.js"
+  "/takamaka/precache-manifest.98611f3b15008e74b35a5435395e7bc5.js"
 );
 
 self.addEventListener('message', (event) => {
